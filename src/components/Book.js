2517236b18275c766dd01b7105781b0b022b7c8d
den-cloud <djpants90@yahoo.com>
2022-03-04 09:39:11 +0200

    Run eslint, stylelint validators & refactor code

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,60 +1,80 @@
 import React from 'react';
+import './Book.css';
 import PropTypes from 'prop-types';
-import styles from '../assets/book.module.css';
+import { useDispatch } from 'react-redux';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 
-const Book = ({
-  id, title, category, removeBook,
-}) => (
-  <div className={styles.lessonPanel}>
-    <div className="Book-Header">
-      <div className={styles.category}>
-        <p>{category}</p>
+import { removeBook } from '../redux/books/books';
+
+function Book({ singleBook }) {
+  const dispatch = useDispatch();
+
+  const deleteHandler = () => {
+    dispatch(removeBook(singleBook.item_id));
+  };
+  return (
+    <>
+      <div className="bookRow" key={singleBook.item_id}>
+        <div className="left-section">
+          <div className="bookInfo">
+            <p className="category">{singleBook.category}</p>
+            <h1 className="title">
+              {singleBook.title}
+            </h1>
+            <p className="author">{singleBook.author}</p>
+          </div>
+          <div className="actions">
+            <ul>
+              <li>
+                <button type="submit">Comments</button>
+              </li>
+              <li>
+                <button type="submit" onClick={deleteHandler}> Remove</button>
+              </li>
+              <li>
+                <button type="submit">Edit</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="border">
+          <div className="middle-section">
+            <div className="progress-div">
+              <CircularProgressbar
+                value={64}
+                strokeWidth={8}
+                styles={buildStyles({
+                  strokeLinecap: 'butt',
+                  pathTransitionDuration: 0.5,
+                  pathColor: '#0290ff',
+                  trailColor: '#DFE3E8',
+                  backgroundColor: '#3e98c7',
+                })}
+              />
+            </div>
+            <div className="text-div">
+              <p className="presentage-text">64%</p>
+              <p className="completed-text">Completed</p>
+            </div>
+          </div>
+        </div>
+        <div className="right-section">
+          <p className="current-chapter">Current Chapter</p>
+          <p className="chapter">Chapter 17</p>
+          <button className="update-btn" type="button">Update progress</button>
+        </div>
       </div>
-      <div className={styles.title}>
-        <h3>{title}</h3>
-      </div>
-      <div className={styles.author}>
-        <p>Suzane Collins</p>
-      </div>
-      <div className="actions">
-        <button type="button" className={`${styles.actionBtn}`}>
-          Comments
-        </button>
-        &nbsp;| &nbsp;
-        <button
-          type="button"
-          className={styles.actionBtn}
-          onClick={() => removeBook({ id, title, category })}
-        >
-          Remove Book
-        </button>
-        &nbsp; | &nbsp;
-        <button type="button" className={styles.actionBtn}>
-          Edit
-        </button>
-      </div>
-    </div>
-    <div className={styles.progressReport}>
-      <p className={styles.Overal2} />
-      <br />
-      <div>
-        <p className={styles.percentageComplete}>60%</p>
-        <p className={styles.complete}>complete</p>
-      </div>
-    </div>
-    <div className="chapter-Info">
-      <p className={styles.currentChapter}>CURRENT CHAPTER</p>
-      <p className={styles.currentLesson}>CHAPTER 3</p>
-      <button type="button" className={styles.button}>UPDATE PROGRESS</button>
-    </div>
-  </div>
-);
+    </>
+  );
+}
 
 Book.propTypes = {
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
-  removeBook: PropTypes.func.isRequired,
+  singleBook: PropTypes.shape({
+    title: PropTypes.string,
+    author: PropTypes.string,
+    category: PropTypes.string,
+    item_id: PropTypes.number,
+  }).isRequired,
 };
 
 export default Book;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers/index';
 import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 
-const booksData = {
-  theBooks: [
-    {
-      id: Math.floor(Math.random() * 100),
-      title: 'Tom and Jerry',
-      category: 'Kids',
-    },
-
-    {
-      id: Math.floor(Math.random() * 100),
-      title: 'As a man Thinketh',
-      category: 'Learning',
-    },
-
-    {
-      id: Math.floor(Math.random() * 100),
-      title: 'Star Wars',
-      category: 'Sci-Fi',
-    },
-  ],
-
-  filter: 'All',
-};
-
-const store = createStore(rootReducer, booksData);
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import Categories from './components/Categories';
+import store from './redux/configureStore/configureStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <div className="container">
+      <Router>
+        <header>
+          <nav>
+            <Link className="logo" to="/"> Bookstore</Link>
+            <Link className="link" to="/">Books</Link>
+            <Link className="link" to="/categories">Categories</Link>
+            <i className="fas fa-user" />
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+
+      </Router>
+    </div>
+  </Provider>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
